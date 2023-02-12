5b8dde344a4ab8944a8213ca66f4e01d6311ae19
amay077 <okuokuoku@gmail.com>
2023-02-12 20:26:56 +0900

    #15 PWA init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +25,7 @@ import { NgxScrollTopModule } from 'ngx-scrolltop';
 import { UiSettingsFrontMatterComponent } from './ui-settings-front-matter/ui-settings-front-matter.component';
 import { UiSettingsRepositoryComponent } from './ui-settings-repository/ui-settings-repository.component';
 import { AboutComponent } from './ui/about/about.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -57,6 +58,12 @@ import { AboutComponent } from './ui/about/about.component';
     MatMenuModule,
     NgxLoadingModule.forRoot({}),
     NgxScrollTopModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
