33d0b22a09885b15e69ef784bc8fe2867a1110af
amay077 <okuokuoku@gmail.com>
2023-02-19 02:51:31 +0900

    app version cheking

--- src/app/ui/about/about.component.ts
+++ src/app/ui/about/about.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
   loading = false;
 
   // @ts-ignore
   app_version = `${window['app_version']}`;
+  latest_app_version = '';
 
   back() {
     history.back();
   }
 
+  async ngOnInit(): Promise<void> {
+    try {
+      const res = await fetch(`version.json?date=${new Date().getTime()}`);
+      const json = await res.json();
+      console.log(json);
+      this.latest_app_version = json.app_version;
+    } catch (error) {
+      console.warn('fetch version.json failed.', error);
+    }
+  }
 }
