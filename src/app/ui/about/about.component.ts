72256c98e403a4b8f2eee0e4c7f738ef7ca8a503
amay077 <okuokuoku@gmail.com>
2023-02-19 20:58:40 +0900

    app version cheking

--- src/app/ui/about/about.component.ts
+++ src/app/ui/about/about.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   loading = false;
+  availableUpdate = false;
 
   // @ts-ignore
   app_version = `${window['app_version']}`;
@@ -22,8 +23,29 @@ export class AboutComponent implements OnInit {
       const json = await res.json();
       console.log(json);
       this.latest_app_version = json.app_version;
+
+      this.availableUpdate = this.latest_app_version != this.app_version;
     } catch (error) {
       console.warn('fetch version.json failed.', error);
     }
+  }
+
+  async updateApps() {
+    await updateApp();
+  }
+}
+
+// PWA を強制更新する(Serviceworker のキャッシュを破棄してリロード)
+// https://www.codit.work/codes/pwqxmywbampxtls2k6jc/
+export async function updateApp() {
+  try {
+    const registrations =
+      await window.navigator.serviceWorker.getRegistrations();
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+    window.location.reload();
+  } catch (error) {
+    console.log(`[pwa-util]update ~ error`, error);
   }
 }
