409a0d9799103e829f68f14e4dd5d61d63680acf
amay077 <okuokuoku@gmail.com>
2023-03-05 20:18:20 +0900

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  app_version: '0.9.0',
+  app_version: '0.9.1',
 
   github_oauth_client_id: 'b8628367d18dae68d2d6',
   github_access_token_api_endpoint: 'https://posteiro-api.netlify.app/.netlify/functions/token',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  app_version: '0.9.0',
+  app_version: '0.9.1',
 
   github_oauth_client_id: '1fcdaeee33ff77b3025b',
   github_access_token_api_endpoint: 'http://localhost:9999/.netlify/functions/token',
